bf5c705bf385643650c662b78399b17b01f773d1
Will Hedges <wshedges@outlook.com>
2023-04-10 15:34:04 -0500

    registered BeanVarietyRepository with Startup class

--- CoffeeShop/Startup.cs
+++ CoffeeShop/Startup.cs
@@ -1,3 +1,4 @@
+using CoffeeShop.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,7 @@ namespace CoffeeShop
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoffeeShop", Version = "v1" });
             });
+            services.AddTransient<IBeanVarietyRepository, BeanVarietyRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
