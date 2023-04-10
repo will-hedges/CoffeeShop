dc3ce1a90cb0bb024c0a6d93c7eb8a1e9033e271
Will Hedges <wshedges@outlook.com>
2023-04-10 16:05:20 -0500

    added Coffee model

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int BeanVarietyId { get; set; }
+        public BeanVariety BeanVariety { get; set; }
+    }
+}
