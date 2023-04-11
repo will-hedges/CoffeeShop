d4a9c2707bc6ba790a59a9f36554f2ced3758e64
Will Hedges <wshedges@outlook.com>
2023-04-11 12:53:41 -0500

    added bean variety form, POSTing * doesn't yet clear form after submit

--- js-app/main.js
+++ js-app/main.js
@@ -16,6 +16,17 @@ function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
 }
 
+function postBeanToAPI(bean) {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(bean)
+    };
+    return fetch(url, fetchOptions);
+}
+
 function displayBeanVarieties(beanVarietyArr) {
     return `
         <h3>Our Beans:</h3>
@@ -30,3 +41,43 @@ function displayBeanVarieties(beanVarietyArr) {
             ).join("")}
         </ul>`
 }
+
+function addBeanForm() {
+    const html = `
+        <h3>Add a New Bean:</h3>
+        <div class="field">
+            <label class="label" for="bean-name">Name</label>
+            <input type="text" name="bean-name" />
+        </div>
+        <div class="field">
+            <label class="label" for="bean-region">Region</label>
+            <input type="text" name="bean-region" />
+        </div>
+        <div class="field">
+            <label class="label" for="bean-notes">Notes</label>
+            <input type="text" name="bean-notes" />
+        </div>
+        <button class="button" id="submit-bean">Add Bean</button>`;
+
+    return html;
+}
+
+beanFormContainer.innerHTML = addBeanForm();
+const addBeanButton = document.querySelector("#submit-bean");
+
+addBeanButton.addEventListener("click", clickEvt => {
+    if (clickEvt.target.id == "submit-bean") {
+        // get the user input fields
+        const beanName = document.querySelector("input[name='bean-name']").value;
+        const beanRegion = document.querySelector("input[name='bean-region']").value;
+        const beanNotes = document.querySelector("input[name='bean-notes']").value;
+        // make the new BeanVariety obj
+        const newBeanObj = {
+            Name: beanName,
+            Region: beanRegion,
+            Notes: beanNotes
+        };
+
+        postBeanToAPI(newBeanObj);
+    }
+})
