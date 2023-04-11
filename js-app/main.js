7f0dde7505767085ebdc783fcdeb933a62883600
Will Hedges <wshedges@outlook.com>
2023-04-11 12:12:21 -0500

    refactored beans list as ul

--- js-app/main.js
+++ js-app/main.js
@@ -1,30 +1,32 @@
+const mainContainer = document.querySelector("#container")
+const beanFormContainer = document.querySelector("#bean-form__container");
+
 const url = "https://localhost:5001/api/beanvariety/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
-        .then(beans => {
-
-            // make a header
-            const newHeader = document.createElement("h3");
-            const headerContent = document.createTextNode("Our Beans:");
-            newHeader.appendChild(headerContent);
-
-            // insert the header before the 'coffee' div
-            const currentDiv = document.getElementById("coffee");
-            document.body.insertBefore(newHeader, currentDiv);
-
-            // display the beans as a 'list' of divs
-            beans.map(bean => {
-                const newDiv = document.createElement("div");
-                const content = document.createTextNode(`${bean.name}`);
-                newDiv.appendChild(content);
-                document.body.insertBefore(newDiv, currentDiv)
-            })
-        })
-    }
-);
+        .then(beanVarieties => {
+            mainContainer.innerHTML = displayBeanVarieties(beanVarieties);
+        }  
+    )
+});
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+function displayBeanVarieties(beanVarietyArr) {
+    return `
+        <h3>Our Beans:</h3>
+        <ul>
+            ${beanVarietyArr.map(
+                (bv) => {
+                    return `
+                    <li class="bean-variety">
+                        ${bv.name} (${bv.region})
+                    </li>`
+                }
+            ).join("")}
+        </ul>`
 }
